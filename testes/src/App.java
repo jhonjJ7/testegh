ae8200866380b06dad486bbadef63eb72e10970d
jhonjJ7 <144070111+jhonjJ7@users.noreply.github.com>
2023-09-05 08:55:26 -0300

    Update App.java

--- testes/src/App.java
+++ testes/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! alterado no github");
+        System.out.println("Hello, World! alterado no github. ok");
     }
 }
